f12ed766b6e3d9df86325bbb62fd216ef9e9ac9b
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-01-23 12:22:22 +0530

    main frame

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Wheel from './wheel';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Wheel />
   </React.StrictMode>
 );
 
